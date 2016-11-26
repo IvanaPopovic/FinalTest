7ceba2ca4ae22793c8035a39e2ba7c08ed76ccf7
IvanaPopovic <2106ivana@gmail.com>
2016-11-26 18:52:04 +0100

    Aktivnosti i manifest

---
+++ app/src/main/java/com/example/android/finaltest/activity/DetailActivity.java
@@ -0,0 +1,11 @@
+package com.example.android.finaltest.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by android on 26.11.16..
+ */
+public class DetailActivity extends AppCompatActivity {
+
+
+}
---
+++ app/src/main/java/com/example/android/finaltest/activity/MainListActivity.java
@@ -0,0 +1,9 @@
+package com.example.android.finaltest.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by android on 26.11.16..
+ */
+public class MainListActivity extends AppCompatActivity {
+}
